3b1f4a69ffcce4858f4aead6388c9123e021e3c5
Ace <riddleyoung@163.com>
2022-05-31 09:31:38 +0800

    第二次修改克隆下来的工程

--- src/com/offcn/demos/Demo02.java
+++ src/com/offcn/demos/Demo02.java
@@ -6,6 +6,7 @@ public class Demo02 {
      * 新文件为红色，是在工作区中的文件
      * 添加到暂存区的文件是绿色的，对暂存区文件做了修改，无需重新添加暂存区
      * 将已经提交到本地库的代码进行修改，文件会变成蓝色
+     * 重新提交到本地库之后，代码又会重新变成黑色
      */
     public static void main(String[] args) {
         System.out.println("新编辑的代码");
